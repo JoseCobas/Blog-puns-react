8f346fb01c442aca2021f7d9c1af7a64c365e5d7
Jose Cobas <julgranen7@gmail.com>
2021-04-09 22:21:50 +0200

    message

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import ManagePuns from './components/ManagePuns'
 
-
+//Remenber at you must open the backend in insomnia. to work with this app
 function App() {
   return (
     <div className="App">
@@ -12,3 +12,4 @@ function App() {
 }
 
 export default App;
+
